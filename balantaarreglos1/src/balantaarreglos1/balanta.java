ad39d17dcbcc7adeaf325c48cb5d2b6b216978d1
Estudiante USC <estudiante@GTACAD.USC>
2022-09-30 17:01:20 -0500

    arreglos1

---
+++ balantaarreglos1/src/balantaarreglos1/balanta.java
@@ -0,0 +1,10 @@
+package balantaarreglos1;
+
+public class balanta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ balantaarreglos1/src/module-info.java
@@ -0,0 +1,2 @@
+module balantaarreglos1 {
+}
